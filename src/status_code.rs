d1dd2469255143465442a33ab12953b62d11742e
Pascal Hertleif <pascal@technocreatives.com>
2019-06-01 17:33:50 +0200

    Add some tests for custom deserialize code

--- src/status_code.rs
+++ src/status_code.rs
@@ -1,5 +1,5 @@
-use std::fmt;
 use serde::{Deserialize, Serialize};
+use std::fmt;
 
 #[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub enum StatusCode {
@@ -39,7 +39,7 @@ impl<'de> Deserialize<'de> for StatusCode {
                 if value < 100 && value > 100 {
                     return Err(E::invalid_value(
                         Unexpected::Signed(value),
-                        &"out of range 100..1000",
+                        &"range 100..1000",
                     ));
                 }
                 Ok(StatusCode::Code(value as u16))
@@ -50,10 +50,7 @@ impl<'de> Deserialize<'de> for StatusCode {
                 E: de::Error,
             {
                 if value.len() != 3 {
-                    return Err(E::invalid_value(
-                        Unexpected::Str(value),
-                        &"expected length 3",
-                    ));
+                    return Err(E::invalid_value(Unexpected::Str(value), &"length 3"));
                 }
 
                 if let Ok(number) = value.parse::<i64>() {
@@ -63,18 +60,17 @@ impl<'de> Deserialize<'de> for StatusCode {
                 if !value.is_ascii() {
                     return Err(E::invalid_value(
                         Unexpected::Str(value),
-                        &"expected ascii, format `\\dXX`",
+                        &"ascii, format `\\dXX`",
                     ));
                 }
 
-                let v = value.as_bytes();
+                let v = value.as_bytes().to_ascii_uppercase();
 
                 match [v[0], v[1], v[2]] {
-                    [n, b'X', b'X'] if n.is_ascii_digit() => Ok(StatusCode::Range((n - b'0') as u16)),
-                    _ => Err(E::invalid_value(
-                        Unexpected::Str(value),
-                        &"expected format `\\dXX`",
-                    )),
+                    [n, b'X', b'X'] if n.is_ascii_digit() => {
+                        Ok(StatusCode::Range((n - b'0') as u16))
+                    }
+                    _ => Err(E::invalid_value(Unexpected::Str(value), &"format `\\dXX`")),
                 }
             }
         }
@@ -89,5 +85,35 @@ impl Serialize for StatusCode {
         S: serde::Serializer,
     {
         serializer.serialize_str(&self.to_string())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::StatusCode;
+    use serde_yaml::from_str;
+
+    #[test]
+    fn deserialize_strings_and_numbers() {
+        assert_eq!(StatusCode::Code(200), from_str("200").unwrap(),);
+        assert_eq!(StatusCode::Code(200), from_str("'200'").unwrap(),);
+    }
+
+    #[test]
+    #[should_panic = "expected length 3"]
+    fn deserialize_invalid_code() {
+        let _: StatusCode = from_str("6666").unwrap();
+    }
+
+    #[test]
+    fn deserialize_ranges() {
+        assert_eq!(StatusCode::Range(2), from_str("2XX").unwrap(),);
+        assert_eq!(StatusCode::Range(4), from_str("'4xx'").unwrap(),);
+    }
+
+    #[test]
+    #[should_panic = "invalid value"]
+    fn deserialize_invalid_range() {
+        let _: StatusCode = from_str("2XY").unwrap();
     }
 }
