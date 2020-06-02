ad87d6d2a3413673b8ab15324d4589495f52d92b
Alexandre Rebert <alex@forallsecure.com>
2020-06-02 18:36:03 -0400

    Add explode field to ParameterData
    From the OpenAPIv3 spec:
    
      explode (true/false) specifies whether arrays and objects should
      generate separate parameters for each array item or object property.

--- src/parameter.rs
+++ src/parameter.rs
@@ -40,6 +40,8 @@ pub struct ParameterData {
     pub example: Option<serde_json::Value>,
     #[serde(default, skip_serializing_if = "IndexMap::is_empty")]
     pub examples: IndexMap<String, ReferenceOr<Example>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub explode: Option<bool>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq)]
