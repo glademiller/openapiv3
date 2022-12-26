fdda8ca39c47adeee4ed55b1383a89ebb6108508
Kurt Wolf <kurtwolfbuilds@gmail.com>
2022-12-25 23:19:37 -0500

    code cleanup

--- Justfile
+++ Justfile
@@ -1,4 +1,5 @@
 set dotenv-load := true
+set positional-arguments
 
 help:
     @just --list --unsorted
--- src/paths.rs
+++ src/paths.rs
@@ -2,6 +2,7 @@ use std::marker::PhantomData;
 
 use crate::*;
 use indexmap::IndexMap;
+use http::Method;
 use serde::{Deserialize, Deserializer, Serialize};
 
 /// Describes the operations available on a single path.
@@ -138,26 +139,6 @@ pub struct Paths {
     pub extensions: IndexMap<String, serde_json::Value>,
 }
 
-pub enum PathMethod {
-    Get,
-    Put,
-    Post,
-    Delete,
-    Patch,
-}
-
-impl PathMethod {
-    pub fn from_str(s: &str) -> Self {
-        match s {
-            "get" => Self::Get,
-            "put" => Self::Put,
-            "post" => Self::Post,
-            "delete" => Self::Delete,
-            "patch" => Self::Patch,
-            _ => panic!("Unknown path method: {}", s),
-        }
-    }
-}
 
 impl Paths {
     /// Iterate over path items.
@@ -173,22 +154,19 @@ impl Paths {
         self.paths.insert(key, ReferenceOr::Item(path_item))
     }
 
-    pub fn insert_operation(&mut self, path: String, method: PathMethod, operation: Operation) -> Option<Operation> {
-        let path_item = self.paths.get_mut(&path);
-        let path_item = match path_item {
-            None => {
-                let path_item = PathItem::default();
-                self.paths.insert(path.clone(), ReferenceOr::Item(path_item));
-                self.paths.get_mut(&path).unwrap().as_mut().unwrap()
-            }
-            Some(x) => x.as_mut().unwrap(),
-        };
+    pub fn insert_operation(&mut self, path: String, method: Method, operation: Operation) -> Option<Operation> {
+        let item = self.paths.entry(path).or_default();
+        let item = item.as_mut().expect("Currently don't support references for PathItem");
         match method {
-            PathMethod::Get => path_item.get.replace(operation),
-            PathMethod::Put => path_item.put.replace(operation),
-            PathMethod::Post => path_item.post.replace(operation),
-            PathMethod::Delete => path_item.delete.replace(operation),
-            PathMethod::Patch => path_item.patch.replace(operation),
+            Method::GET => item.get.replace(operation),
+            Method::PUT => item.put.replace(operation),
+            Method::POST => item.post.replace(operation),
+            Method::DELETE => item.delete.replace(operation),
+            Method::PATCH => item.patch.replace(operation),
+            Method::HEAD => item.head.replace(operation),
+            Method::OPTIONS => item.options.replace(operation),
+            Method::TRACE => item.trace.replace(operation),
+            _ => panic!("Unsupported method: {:?}", method),
         }
     }
 }
