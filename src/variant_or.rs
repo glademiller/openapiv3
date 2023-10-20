31230cb8f1dd9c370d00e6c4b73b53705dddcff2
Adam Leventhal <ahl@oxide.computer>
2023-10-20 14:12:13 -0700

    impl From and FromStr functions in some tactical places (#57)

--- src/variant_or.rs
+++ src/variant_or.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
@@ -5,6 +7,18 @@ use serde::{Deserialize, Serialize};
 pub enum VariantOrUnknown<T> {
     Item(T),
     Unknown(String),
+}
+
+impl<T> From<String> for VariantOrUnknown<T>
+where
+    T: FromStr,
+{
+    fn from(s: String) -> Self {
+        match T::from_str(&s) {
+            Ok(t) => Self::Item(t),
+            Err(_) => Self::Unknown(s),
+        }
+    }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
@@ -27,5 +41,41 @@ impl<T> VariantOrUnknownOrEmpty<T> {
 impl<T> Default for VariantOrUnknownOrEmpty<T> {
     fn default() -> Self {
         VariantOrUnknownOrEmpty::Empty
+    }
+}
+
+impl<T> From<Option<String>> for VariantOrUnknownOrEmpty<T>
+where
+    T: FromStr,
+{
+    fn from(v: Option<String>) -> Self {
+        match v {
+            Some(s) => match T::from_str(&s) {
+                Ok(t) => VariantOrUnknownOrEmpty::Item(t),
+                Err(_) => VariantOrUnknownOrEmpty::Unknown(s),
+            },
+            None => VariantOrUnknownOrEmpty::Empty,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{StringFormat, VariantOrUnknownOrEmpty};
+
+    #[test]
+    fn test_variant_from() {
+        assert_eq!(
+            VariantOrUnknownOrEmpty::<StringFormat>::from(None),
+            VariantOrUnknownOrEmpty::Empty,
+        );
+        assert_eq!(
+            VariantOrUnknownOrEmpty::<StringFormat>::from(Some("date".to_string())),
+            VariantOrUnknownOrEmpty::Item(StringFormat::Date),
+        );
+        assert_eq!(
+            VariantOrUnknownOrEmpty::<StringFormat>::from(Some("yolo".to_string())),
+            VariantOrUnknownOrEmpty::Unknown("yolo".to_string()),
+        );
     }
 }
